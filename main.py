6107895c255046895f6325f67d7638761545ff9e
birlamchinom-prog <birlamchinomir@gmail.com>
2026-07-02 09:46:07 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+
+print('"Nexia", "Tico", \'Damas\' ko\'rganlar qilar havas')
+
+print("-" * 40)
+
+print(f"5 ning 4-darajasi {5**4}")
+
+print(f"22 ni 4 ga bo'lganda qoldiq {22 % 4} qoladi")
+
+a = 125
+S_kvadrat = a**2
+P_kvadrat = 4 * a
+print(f"Tomonlari 125 ga teng kvadratning yuzi {S_kvadrat}, perimetri {P_kvadrat}")
+
+d = 12
+r = d / 2
+pi = 3.14
+S_doira = pi * (r**2)
+print(f"Diametri 12 ga teng bo'lgan doiraning yuzi {S_doira}")
